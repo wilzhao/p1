6d326ad322cbee3150ca72fa80126e93fabcef8e
wilzhao <wzhaomt454@gmail.com>
2014-06-29 02:16:07 -0400

    updated index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>
+
+</title>
+</head>
+
+<body>
+In 2+ sentences describe who you are and what you do.
+<br>
+Why are you taking this course?
+<br>
+What previous experience do you have with HTML/CSS?
+<br>
+What previous experience do you have with programming?
+<br>
+Have you worked with PHP before? If so, to what extent?
+<br>
+Are you working on a Mac, PC, or some other platform?
+<br>
+Anything else we should know?
+
+</body>
+</html>
