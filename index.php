8b1d33226d6b31b6d72215aa14cfb2f4fcb13800
wilzhao <wzhaomt454@gmail.com>
2014-06-29 21:18:20 -0400

    updated index

--- index.php
+++ index.php
@@ -10,27 +10,27 @@
 <h2>
 In 2+ sentences describe who you are and what you do.
 </h2>
-<br>
+I am a student at Stuyvesant High School.
 <h2>
 Why are you taking this course?
 </h2>
-<br>
+I am taking this course because I wanted to enrich my understanding of websites and make websites of my own look and work better.
 <h2>
 What previous experience do you have with HTML/CSS?
 </h2>
-<br>
+I have taken a few years of computer science at my high school and have worked with HTML and CSS for two years.
 <h2>
 What previous experience do you have with programming?
 </h2>
-<br>
+In the courses I've taken in school, I have used Scheme, Netlogo, Python and Java and most recently been using Java for a year.
 <h2>
 Have you worked with PHP before? If so, to what extent?
 </h2>
-<br>
+I have not worked with PHP before.
 <h2>
 Are you working on a Mac, PC, or some other platform?
 </h2>
-<br>
+I am working on a PC.
 <h2>
 Anything else we should know?
 </h2>
